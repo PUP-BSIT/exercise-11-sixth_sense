ac33988f4e406c28a8d98f5f1e564ea97d32e572
ILAGAN, ANDREI JIREH M. <ilaganandreijireh@gmail.com>
2024-04-27 12:47:04 +0800

    add comment and new changes in html file

--- ilagan_andreijireh/myscript.js
+++ ilagan_andreijireh/myscript.js
@@ -1,13 +1,13 @@
 function validateInput() {
-    let inputField = document.getElementById("comment");
-    let submitBtn = document.getElementById("submit_btn");
+  let inputField = document.getElementById("comment");
+  let submitBtn = document.getElementById("submit_btn");
   
-    if (inputField.value.trim().length > 0) {
-      submitBtn.disabled = false;
-      submitBtn.style.color = "blue";
-    } else {
-      submitBtn.disabled = true;
-      submitBtn.style.color = "";
-    }
+  if (inputField.value.trim().length > 0) {
+    submitBtn.disabled = false;
+    submitBtn.style.color = "blue";
+  } else {
+    submitBtn.disabled = true;
+    submitBtn.style.color = "";
   }
+}
   
