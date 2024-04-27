f17d74c783f90ffa3bd69e148a1c9fe140b3b98f
ILAGAN, ANDREI JIREH M. <ilaganandreijireh@gmail.com>
2024-04-27 23:56:31 +0800

    add new changes in html file and renamed .js file

--- ilagan_andreijireh/script.js
+++ ilagan_andreijireh/script.js
@@ -1,8 +1,7 @@
 function validateInput() {
   let inputField = document.getElementById("comment");
   let nameField = document.getElementById("name");
-  let submitBtn = document.getElementById("submit-btn");
-
+  let submitBtn = document.getElementById("submit_btn");
   if (inputField.value.trim().length > 0 && nameField.value.trim().length > 0) {
     submitBtn.disabled = false;
   } else {
