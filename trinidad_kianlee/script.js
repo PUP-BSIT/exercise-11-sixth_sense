45920ca1c1cfbbd9896c7574ccdbf1a183980b27
Trinidad, Kian Lee B. <trinidadkianleeb@gmail.com>
2024-04-27 18:36:58 +0800

    update html and js

--- trinidad_kianlee/script.js
+++ trinidad_kianlee/script.js
@@ -1,6 +1,6 @@
 function checkComment() {
-  let userComment = document.getElementById("userComment");
-  let commentButton = document.getElementById("comment_Button");
+  let userComment = document.getElementById("user_comment");
+  let commentButton = document.getElementById("comment_button");
 
   if (userComment.value.trim().length > 0) {
     commentButton.disabled = false;
