94ba815c20ebd6b154ec75430ba415c0f9ac8657
armedillaalliyah <armedillaalliyah@gmail.com>
2024-04-27 20:40:05 +0800

    update armedilla_alliyah_samantha folder

--- armedilla_alliyah_samantha/myjs.js
+++ armedilla_alliyah_samantha/myjs.js
@@ -1,13 +1,12 @@
 function validateInput() {
-    let inputField = document.getElementById("comment", "name");
-    let submitBtn = document.getElementById("submit_button");
-  
-    if (inputField.value.trim().length > 0) {
-      submitBtn.disabled = false;
-      submitBtn.style.color = "dark blue";
-    } else {
-      submitBtn.disabled = true;
-      submitBtn.style.color = "";
-    }
+  let inputField = document.getElementById("comment", "name");
+  let submitBtn = document.getElementById("submit_button");
+
+  if (inputField.value.trim().length > 0) {
+    submitBtn.disabled = false;
+    submitBtn.style.color = "dark blue";
+  } else {
+    submitBtn.disabled = true;
+    submitBtn.style.color = "";
+  }
 }
-  
